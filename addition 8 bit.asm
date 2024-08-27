a2aa383505b171b7b3adf219e20eadd95f787047
sarayuthati <123063274+sarayuthati@users.noreply.github.com>
2024-08-27 12:06:53 +0530

    Add files via upload

---
+++ addition 8 bit.asm
@@ -0,0 +1,29 @@
+num1 db 18h
+num2 db 18h
+start:
+mov al,num1
+add al,num2
+mov bl,al 
+mov ah,al
+and ah,0F0h
+shr ah,4
+add ah,30h
+cmp ah,39h
+jle print_first_digit
+add ah,7
+print_first_digit:
+mov dl,ah
+mov ah,02h
+int 21h
+mov ah,bl
+and ah,0Fh
+add ah,30h
+cmp ah,39h
+jle print_second_digit
+add ah,7
+print_second_digit:
+mov dl,ah
+mov ah,02h
+int 21h
+mov ah,4Ch
+int 21h
